ebdf2dceda458bf5f1453085f5644dcf893c47c7
Clar2Verata <Clar2Verata@tuta.io>
2025-11-16 20:05:55 -0500

    markview get

---
+++ config/utils/markview.nix
@@ -0,0 +1,47 @@
+{ config, lib, ... }:
+{
+  options = {
+    markview.enable = lib.mkEnableOption "Enable Markview module";
+  };
+  config = lib.mkIf config.markview.enable {
+
+  plugins.markview =
+    let
+      filetypes = [
+        "latex"
+        "markdown"
+        "md"
+        "norg"
+        "org"
+        "quarto"
+        "rmd"
+        "typst"
+        "vimwiki"
+      ];
+    in
+    {
+      
+      lazyLoad.settings.ft = filetypes;
+
+      settings = {
+        preview = {
+          inherit filetypes;
+
+          ignore_buftypes = [ ];
+
+          };
+      };
+    };
+
+  keymaps = lib.mkIf config.plugins.markview.enable [
+    {
+      mode = "n";
+      key = "<leader>um";
+      action = "<cmd>Markview toggle<CR>";
+      options = {
+        desc = "Toggle Markdown Preview";
+      };
+    }
+  ];
+  };
+}
